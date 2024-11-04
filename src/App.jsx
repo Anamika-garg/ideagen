8185c810b6c4533c1ed70388e21b860782cf23fb
Anamika Garg <anamikagarg2906@gmail.com>
2024-11-04 08:27:02 +0530

    routing changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import SignUp from './components/SignUp'
 import Login from './components/Login'
 import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import Layout from './components/Layout'
+import SetupGuide from "./components/SetupGuide";
+import ProductSection from "./components/ProductSection";
+import AnalyticsSection from "./components/AnalyticsSection";
+import Theme from "./components/Theme";
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
+     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>} />
-        <Route path="/product" element={<Layout/>} />
-        <Route path="/theme" element={<Layout/>} />
-        <Route path="/analytics" element={<Layout/>} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SetupGuide />} />
+          <Route path="product" element={<ProductSection />} />
+          <Route path="analytics" element={<AnalyticsSection />} />
+          <Route path="theme" element={<Theme />} />
+        </Route>
         <Route path="signup" element={<SignUp />} />
         <Route path="login" element={<Login />} />
       </Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,26 +7,26 @@ const Sidebar = () => {
     <div className="bg-white w-64 p-4 space-y-6">
       <h2 className="text-2xl font-bold">Logo</h2>
       <nav className="space-y-4">
-        <a href="/" className="flex items-center space-x-2 text-gray-700">
+        <Link to={"/"} className="flex items-center space-x-2 text-gray-700">
           <HomeIcon className="h-5 w-5" />
           <span>Home</span>
-        </a>
-        {/* <a href="#" className="flex items-center space-x-2 text-gray-700">
+        </Link>
+        {/* <Link to={"#" className="}flex items-center space-x-2 text-gray-700">
           <ShoppingCartIcon className="h-5 w-5" />
           <span>Orders</span>
         </a> */}
-        <a href="/product" className="flex items-center space-x-2 text-gray-700">
+        <Link to={"/product"} className="flex items-center space-x-2 text-gray-700">
           <CubeIcon className="h-5 w-5" />
           <span>Products</span>
-        </a>
-        <a href="/theme" className="flex items-center space-x-2 text-gray-700">
+        </Link>
+        <Link to={"/theme"} className="flex items-center space-x-2 text-gray-700">
           <CogIcon className="h-5 w-5" />
           <span>Choose Themes</span>
-        </a>
-        <a href="/analytics" className="flex items-center space-x-2 text-gray-700">
+        </Link>
+        <Link to={"/analytics"} className="flex items-center space-x-2 text-gray-700">
           <ChartBarIcon className="h-5 w-5" />
           <span>Analytics</span>
-        </a>
+        </Link>
       </nav>
     </div>
   );
